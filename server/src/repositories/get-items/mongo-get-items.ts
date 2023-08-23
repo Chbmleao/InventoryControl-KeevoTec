b67433e0dfc8b55f2ef21818509d6e3120edb9b1
Carlos Leão <caikleao@gmail.com>
2023-08-23 11:18:09 -0300

    Add id to item

--- server/src/repositories/get-items/mongo-get-items.ts
+++ server/src/repositories/get-items/mongo-get-items.ts
@@ -1,15 +1,17 @@
 import { IGetItemsRepository } from "../../controllers/get-items/protocols";
+import { MongoClient } from "../../database/mongo";
 import { Item } from "../../models/item";
 
 export class MongoGetItemsRepository implements IGetItemsRepository {
   async getItems(): Promise<Item[]> {
-    return [
-      {
-        id: 1,
-        description: "Computer",
-        quantity: 10,
-        measureUnit: "unit",
-      },
-    ];
+    const items = await MongoClient.db
+      .collection<Omit<Item, "id">>("items")
+      .find({})
+      .toArray();
+
+    return items.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
