f1cb83b1c6bc32389777b6bbca9e1427c5782cfb
Carlos Leão <caikleao@gmail.com>
2023-08-23 21:29:54 -0300

    Add delete item controller

---
+++ server/src/controllers/delete-item/delete-item.ts
@@ -0,0 +1,33 @@
+import { Item } from "../../models/item";
+import { IDeleteItemRepository } from "./protocols";
+import { HttpRequest, HttpResponse } from "../protocols";
+import { IDeleteItemController } from "./protocols";
+
+export class DeleteItemController implements IDeleteItemController {
+  constructor(private readonly deleteItemRepository: IDeleteItemRepository) {}
+
+  async handle(httpRequest: HttpRequest<Item>): Promise<HttpResponse<Item>> {
+    try {
+      const id = httpRequest?.params?.id;
+
+      if (!id) {
+        return {
+          statusCode: 400,
+          body: "Missing item id",
+        };
+      }
+
+      const item = await this.deleteItemRepository.deleteItem(id);
+
+      return {
+        statusCode: 200,
+        body: item,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong",
+      };
+    }
+  }
+}
--- server/src/controllers/delete-item/protocols.ts
+++ server/src/controllers/delete-item/protocols.ts
@@ -1,4 +1,9 @@
 import { Item } from "../../models/item";
+import { HttpRequest, HttpResponse } from "../protocols";
+
+export interface IDeleteItemController {
+  handle(httpRequest: HttpRequest<Item>): Promise<HttpResponse<Item>>;
+}
 
 export interface IDeleteItemRepository {
   deleteItem(id: string): Promise<Item>;
