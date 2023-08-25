a83afa886ead789bd4439c2c8f43ed466aef5b8a
Carlos Leão <caikleao@gmail.com>
2023-08-25 10:53:58 -0300

    Create list-items component

--- client/src/app/items/items.module.ts
+++ client/src/app/items/items.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ListItemsComponent } from './list';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ListItemsComponent],
+  imports: [CommonModule],
 })
-export class ItemsModule { }
+export class ItemsModule {}
