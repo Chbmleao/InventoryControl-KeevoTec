e73d7d9af943fb9b0714d2b7adee947044f625ff
Carlos Leão <caikleao@gmail.com>
2023-08-25 15:13:15 -0300

    Add HTML interface and CSS style sheet

--- client/src/app/items/items.module.ts
+++ client/src/app/items/items.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ItemService, ItemHttpService } from './shared';
 import { ListItemsComponent } from './list';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { InsertItemComponent } from './insert-item/insert-item.component';
 
 @NgModule({
-  declarations: [ListItemsComponent],
-  imports: [CommonModule],
+  declarations: [ListItemsComponent, InsertItemComponent],
+  imports: [CommonModule, FontAwesomeModule],
   providers: [ItemService, ItemHttpService],
 })
 export class ItemsModule {}
