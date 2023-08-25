a4e92b1f6d43f10c6ab59a553a00ec45c0296b0a
Carlos Leão <caikleao@gmail.com>
2023-08-25 10:48:24 -0300

    Create items module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemsModule } from './items';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ItemsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
