956fa35aef8159ae6ff6d3234a161870a56434e2
Carlos Leão <caikleao@gmail.com>
2023-08-25 11:50:30 -0300

    Add list items component

--- client/src/app/items/list/list-items.component.ts
+++ client/src/app/items/list/list-items.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { ItemService, Item } from '../shared';
 
 @Component({
   selector: 'app-list-items',
   templateUrl: './list-items.component.html',
-  styleUrls: ['./list-items.component.css']
+  styleUrls: ['./list-items.component.css'],
 })
-export class ListItemsComponent {
+export class ListItemsComponent implements OnInit {
+  items$: Observable<Item[]>;
 
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    this.items$ = this.getAll();
+    console.log(this.items$);
+  }
+
+  getAll(): Observable<Item[]> {
+    return this.itemService.getAllItems();
+  }
 }
