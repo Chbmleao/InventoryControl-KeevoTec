81ae7e873df1bb23a3578e806d2310b4dc54ff71
Carlos Leão <caikleao@gmail.com>
2023-08-25 11:20:09 -0300

    Add item services to items module providers

--- client/src/app/items/items.module.ts
+++ client/src/app/items/items.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ItemService, ItemHttpService } from './shared';
 import { ListItemsComponent } from './list';
 
 @NgModule({
   declarations: [ListItemsComponent],
   imports: [CommonModule],
+  providers: [ItemService, ItemHttpService],
 })
 export class ItemsModule {}
