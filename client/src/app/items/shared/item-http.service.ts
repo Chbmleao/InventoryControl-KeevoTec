1829a000d95ec03a0243a2bc9134307d213918d7
Carlos Leão <caikleao@gmail.com>
2023-08-25 11:17:42 -0300

    Create item-http service and add getAllItems function

---
+++ client/src/app/items/shared/item-http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Item } from '.';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ItemHttpService {
+  private apiUrl = 'https://localhost:8000/items';
+
+  constructor(private http: HttpClient) {}
+
+  getAllItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(`${this.apiUrl}`);
+  }
+}
