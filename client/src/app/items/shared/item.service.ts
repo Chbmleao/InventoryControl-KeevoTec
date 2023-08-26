38eba570ae7097fcad7a51cd099bfb69145db4d0
Carlos Leão <caikleao@gmail.com>
2023-08-26 11:19:11 -0300

    Add insert, update and delete functions

--- client/src/app/items/shared/item.service.ts
+++ client/src/app/items/shared/item.service.ts
@@ -13,4 +13,16 @@ export class ItemService {
   getAllItems(): Observable<Item[]> {
     return this.itemHttpService.getAllItems();
   }
+
+  insert(item: Item): Observable<Item> {
+    return this.itemHttpService.post(item);
+  }
+
+  update(item: Item): Observable<Item> {
+    return this.itemHttpService.patch(item);
+  }
+
+  delete(id: string): Observable<Item> {
+    return this.itemHttpService.delete(id);
+  }
 }
