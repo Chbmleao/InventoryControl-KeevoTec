d82a6f7c1b0602414f4763e01b21541e678f270e
Carlos Leão <caikleao@gmail.com>
2023-08-30 10:18:38 -0300

    Refactor: modularize routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
+
 import { MongoClient } from "./database/mongo";
-import { GetItemsController } from "./controllers/get-items/get-items";
-import { MongoGetItemsRepository } from "./repositories/get-items/mongo-get-items";
-import { GetItemByIdController } from "./controllers/get-item-id/get-item-id";
-import { MongoGetItemByIdRepository } from "./repositories/get-item-id/mongo-get-item-id";
-import { MongoCreateItemRepository } from "./repositories/create-item/mongo-create-item";
-import { CreateItemController } from "./controllers/create-item/create-item";
-import { MongoUpdateItemRepository } from "./repositories/update-item/mongo-update-item";
-import { UpdateItemController } from "./controllers/update-item/update-item";
-import { MongoDeleteItemRepository } from "./repositories/delete-item/mongo-delete-user";
-import { DeleteItemController } from "./controllers/delete-item/delete-item";
+import itemRoutes from "./routes";
 
 const main = async () => {
   config();
@@ -19,78 +11,13 @@ const main = async () => {
   const app = express();
 
   app.use(cors());
-
   app.use(express.json());
 
   await MongoClient.connect();
 
-  app.get("/items", async (req, res) => {
-    const mongoGetItemsRepository = new MongoGetItemsRepository();
-    const getItemsController = new GetItemsController(mongoGetItemsRepository);
-
-    const { body, statusCode } = await getItemsController.handle();
-
-    res.status(statusCode).send(body);
-  });
-
-  app.get("/items/:id", async (req, res) => {
-    const mongoGetItemByIdRepository = new MongoGetItemByIdRepository();
-    const getItemByIdController = new GetItemByIdController(
-      mongoGetItemByIdRepository
-    );
-
-    const { body, statusCode } = await getItemByIdController.handle({
-      params: req.params,
-    });
-
-    res.status(statusCode).send(body);
-  });
-
-  app.post("/items", async (req, res) => {
-    const mongoCreateItemRepository = new MongoCreateItemRepository();
-
-    const createItemController = new CreateItemController(
-      mongoCreateItemRepository
-    );
-
-    const { body, statusCode } = await createItemController.handle({
-      body: req.body,
-    });
-
-    res.status(statusCode).send(body);
-  });
-
-  app.patch("/items/:id", async (req, res) => {
-    const mongoUpdateItemRepository = new MongoUpdateItemRepository();
-
-    const updateItemController = new UpdateItemController(
-      mongoUpdateItemRepository
-    );
-
-    const { body, statusCode } = await updateItemController.handle({
-      body: req.body,
-      params: req.params,
-    });
-
-    res.status(statusCode).send(body);
-  });
-
-  app.delete("/items/:id", async (req, res) => {
-    const mongoDeleteItemRepository = new MongoDeleteItemRepository();
-
-    const deleteItemController = new DeleteItemController(
-      mongoDeleteItemRepository
-    );
-
-    const { body, statusCode } = await deleteItemController.handle({
-      params: req.params,
-    });
-
-    res.status(statusCode).send(body);
-  });
+  itemRoutes(app);
 
   const port = process.env.PORT || 8000;
-
   app.listen(port, () => console.log(`Listening on port ${port}!`));
 };
 
---
+++ server/src/routes/item-routes.ts
@@ -0,0 +1,77 @@
+import express from "express";
+
+import { GetItemsController } from "../controllers/get-items/get-items";
+import { MongoGetItemsRepository } from "../repositories/get-items/mongo-get-items";
+import { GetItemByIdController } from "../controllers/get-item-id/get-item-id";
+import { MongoGetItemByIdRepository } from "../repositories/get-item-id/mongo-get-item-id";
+import { MongoCreateItemRepository } from "../repositories/create-item/mongo-create-item";
+import { CreateItemController } from "../controllers/create-item/create-item";
+import { MongoUpdateItemRepository } from "../repositories/update-item/mongo-update-item";
+import { UpdateItemController } from "../controllers/update-item/update-item";
+import { MongoDeleteItemRepository } from "../repositories/delete-item/mongo-delete-user";
+import { DeleteItemController } from "../controllers/delete-item/delete-item";
+
+const itemRoutes = (app: express.Application) => {
+  const router = express.Router();
+
+  router.get("/items", async (req, res) => {
+    const mongoGetItemsRepository = new MongoGetItemsRepository();
+    const getItemsController = new GetItemsController(mongoGetItemsRepository);
+
+    const { body, statusCode } = await getItemsController.handle();
+    res.status(statusCode).send(body);
+  });
+
+  router.get("/items/:id", async (req, res) => {
+    const mongoGetItemByIdRepository = new MongoGetItemByIdRepository();
+    const getItemByIdController = new GetItemByIdController(
+      mongoGetItemByIdRepository
+    );
+
+    const { body, statusCode } = await getItemByIdController.handle({
+      params: req.params,
+    });
+    res.status(statusCode).send(body);
+  });
+
+  router.post("/items", async (req, res) => {
+    const mongoCreateItemRepository = new MongoCreateItemRepository();
+    const createItemController = new CreateItemController(
+      mongoCreateItemRepository
+    );
+
+    const { body, statusCode } = await createItemController.handle({
+      body: req.body,
+    });
+    res.status(statusCode).send(body);
+  });
+
+  router.patch("/items/:id", async (req, res) => {
+    const mongoUpdateItemRepository = new MongoUpdateItemRepository();
+    const updateItemController = new UpdateItemController(
+      mongoUpdateItemRepository
+    );
+
+    const { body, statusCode } = await updateItemController.handle({
+      body: req.body,
+      params: req.params,
+    });
+    res.status(statusCode).send(body);
+  });
+
+  router.delete("/items/:id", async (req, res) => {
+    const mongoDeleteItemRepository = new MongoDeleteItemRepository();
+    const deleteItemController = new DeleteItemController(
+      mongoDeleteItemRepository
+    );
+
+    const { body, statusCode } = await deleteItemController.handle({
+      params: req.params,
+    });
+    res.status(statusCode).send(body);
+  });
+
+  app.use(router);
+};
+
+export default itemRoutes;
