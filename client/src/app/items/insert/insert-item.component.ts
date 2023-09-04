e907ca83890486b11b4d9c2b5f1bf4d45549b9cd
Carlos Leão <caikleao@gmail.com>
2023-09-03 22:57:22 -0300

    Add insert item with existing description verification

--- client/src/app/items/insert/insert-item.component.ts
+++ client/src/app/items/insert/insert-item.component.ts
@@ -28,18 +28,59 @@ export class InsertItemComponent implements OnInit {
     this.item = new Item('', '', 0, '');
   }
 
-  insert(): void {
+  async insert() {
     if (this.formItem.form.valid) {
-      this.itemService.insert(this.item).subscribe(
-        () => {
-          console.log('Item inserted successfully');
-          this.closePage(true);
-        },
-        (error) => {
-          console.error('Error inserting item:', error);
+      let existingId = '';
+
+      try {
+        const items = await this.itemService.getAllItems().toPromise();
+
+        items.forEach((item) => {
+          if (item.description === this.item.description) {
+            existingId = item.id;
+          }
+        });
+      } catch (error) {
+        console.error('Error getting items:', error);
+      }
+
+      if (existingId) {
+        if (
+          confirm(
+            `Já existe um item com a descrição "${this.item.description}", você quer substituí-lo com os dados informados?`
+          )
+        ) {
+          const updatedItem = this.item;
+          updatedItem.id = existingId;
+
+          this.itemService.update(updatedItem).subscribe(
+            () => {
+              console.log('Item updated successfully');
+              this.closePage(true);
+            },
+            (error) => {
+              console.error('Error updating item:', error);
+            }
+          );
+        } else {
+          this.insertAux();
         }
-      );
+      } else {
+        this.insertAux();
+      }
     }
+  }
+
+  insertAux() {
+    this.itemService.insert(this.item).subscribe(
+      () => {
+        console.log('Item inserted successfully');
+        this.closePage(true);
+      },
+      (error) => {
+        console.error('Error inserting item:', error);
+      }
+    );
   }
 
   closePage(reload: boolean): void {
